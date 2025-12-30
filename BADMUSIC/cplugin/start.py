b4d4a9f0dfd7590b66dea7c8fdd386de878584bd
UCHIHABOTS <txkuze@gmail.com>
2025-12-30 22:18:17 +0530

    Update start.py

--- BADMUSIC/cplugin/start.py
+++ BADMUSIC/cplugin/start.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from BADMUSIC import app
+from UCHIHA import app
 
 @Client.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
@@ -9,4 +9,5 @@ async def start_message(client: Client, message: Message):
         "ɪ'ᴍ ʏᴏᴜʀ ʀᴇᴀᴄᴛɪᴏɴ ʙᴏᴛ! ɪ'ʟʟ ʀᴇᴀᴄᴛ ᴛᴏ ᴇᴠᴇʀʏ ᴍᴇꜱꜱᴀɢᴇ ɪɴ ɢʀᴏᴜᴘꜱ, ᴄʜᴀɴɴᴇʟꜱ, ᴀɴᴅ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛꜱ ᴡɪᴛʜ ᴀ 👍 ᴇᴍᴏᴊɪ.\n\n"
         "ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴏʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴡᴀᴛᴄʜ ᴍᴇ ɪɴ ᴀᴄᴛɪᴏɴ! 🚀\n\n"
         "ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ /clone😁"
+        "ᴍᴀᴅᴇ ʙY💗 [⚓˹ᴧɴσɴʏᴍσᴜs ⇾ ɴᴇᴏᴄᴏᴅᴇʀ - ❕](https://t.me/uchiha_owner)"
     )
