398b25d6aeddb36c0e6c90b21f184307a10340c1
UCHIHABOTS <txkuze@gmail.com>
2025-12-30 23:26:29 +0530

    Update privacy.py

--- BADMUSIC/plugins/bot/privacy.py
+++ BADMUSIC/plugins/bot/privacy.py
@@ -3,7 +3,7 @@ from pyrogram.enums import ParseMode
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 import config
-from BADMUSIC import app
+from UCHIHA import app
 
 
 @app.on_message(filters.command("privacy"))
