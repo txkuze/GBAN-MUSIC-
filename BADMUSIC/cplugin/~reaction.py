c228d55ba6b667d94f3b658430089baea37e4cf6
UCHIHABOTS <txkuze@gmail.com>
2025-12-30 22:19:06 +0530

    Update ~reaction.py

--- BADMUSIC/cplugin/~reaction.py
+++ BADMUSIC/cplugin/~reaction.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import random
 
-EMOJIS = ["🥰", "❤️", "😁", "💋", "😱", "🤣", "😘", "❤️‍🔥", "👌", "🫡", "😍"]
+EMOJIS = ["❄️", "🎊", "😁", "📢", "😱", "🤣", "👀", "❤️‍🔥", "👌", "🫡", "⚡"]
 
 @Client.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
