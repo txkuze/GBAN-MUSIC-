8140db7b7fe0a0a6b68a83113bf2871f87b26757
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 21:52:24 +0530

    Update __init__.py

--- BADMUSIC/utils/welcome/__init__.py
+++ BADMUSIC/utils/welcome/__init__.py
@@ -3,8 +3,8 @@ from sys import exit as exiter
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 
-from BADMUSIC.logging import LOGGERR
-from BADMUSIC.logging import *
+from UCHIHA.logging import LOGGERR
+from UCHIHA.logging import *
 from config import MONGO_DB_URI, DB_NAME
 
 try:
