b9f63a14f5d472510f1675fd2febbb5afd7a227e
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 21:50:13 +0530

    Update queue.py

--- BADMUSIC/utils/stream/queue.py
+++ BADMUSIC/utils/stream/queue.py
@@ -6,7 +6,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from BADMUSIC.misc import db
+from UCHIHA.misc import db
 
 
 async def put_queue(
