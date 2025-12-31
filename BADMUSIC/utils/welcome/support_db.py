9f45ee13f51c7572ad5a987a4537a6b5322f4e83
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 21:57:58 +0530

    Update support_db.py

--- BADMUSIC/utils/welcome/support_db.py
+++ BADMUSIC/utils/welcome/support_db.py
@@ -1,7 +1,7 @@
 from threading import RLock
 
-from BADMUSIC import LOGGER
-from BADMUSIC.utils.welcome import MongoDB
+from UCHIHA import LOGGER
+from UCHIHA.utils.welcome import MongoDB
 
 INSERTION_LOCK = RLock()
 
