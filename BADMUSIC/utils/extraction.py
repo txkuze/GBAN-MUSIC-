b82151066008084f2d90586242e2039f83e96912
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 22:00:55 +0530

    Update extraction.py

--- BADMUSIC/utils/extraction.py
+++ BADMUSIC/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from BADMUSIC import app
+from UCHIHA import app
 
 
 async def extract_user(m: Message) -> User:
