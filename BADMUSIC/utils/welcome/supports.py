34d97dcb839f2dbd58e562d478e56fbf2a3e4b72
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 21:58:27 +0530

    Update supports.py

--- BADMUSIC/utils/welcome/supports.py
+++ BADMUSIC/utils/welcome/supports.py
@@ -1,6 +1,6 @@
 from config import OWNER_ID
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.welcome.support_db import SUPPORTS
+from UCHIHA.misc import SUDOERS
+from UCHIHA.utils.welcome.support_db import SUPPORTS
 
 
 async def load_support_users():
