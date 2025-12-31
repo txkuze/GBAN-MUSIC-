3ec40d9b08095b9191c652391da4e6f6e9f4d302
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 22:28:16 +0530

    Update autoend.py

--- BADMUSIC/plugins/sudo/autoend.py
+++ BADMUSIC/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
 from strings import get_command
-from BADMUSIC import app
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.database import autoend_off, autoend_on
+from UCHIHA import app
+from UCHIHA.misc import SUDOERS
+from UCHIHA.utils.database import autoend_off, autoend_on
 
 # Commands
 AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
