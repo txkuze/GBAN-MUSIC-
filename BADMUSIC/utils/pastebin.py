974c9c62f99b0b6c2fe98c31804a03439cc51171
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 22:14:31 +0530

    Update pastebin.py

--- BADMUSIC/utils/pastebin.py
+++ BADMUSIC/utils/pastebin.py
@@ -16,7 +16,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def BADbin(text):
+async def ARJUNbin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
