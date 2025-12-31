f44f8cd0c4ca2bd395e458f6cab8c90851d836b9
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 21:57:24 +0530

    Update string.py

--- BADMUSIC/utils/welcome/string.py
+++ BADMUSIC/utils/welcome/string.py
@@ -6,8 +6,8 @@ from typing import List
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, Message
 
-from BADMUSIC import TIME_ZONE
-from BADMUSIC.utils.welcome.parser import escape_markdown
+from UCHIHA import TIME_ZONE
+from UCHIHA.utils.welcome.parser import escape_markdown
 
 BTN_URL_REGEX = compile_re(r"(\[([^\[]+?)\]\(buttonurl:(?:/{0,2})(.+?)(:same)?\))")
 
