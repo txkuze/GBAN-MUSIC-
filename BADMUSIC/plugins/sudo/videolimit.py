92d9636f51fc19a445c88f727d1e2392b3543187
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 22:47:52 +0530

    Update videolimit.py

--- BADMUSIC/plugins/sudo/videolimit.py
+++ BADMUSIC/plugins/sudo/videolimit.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from BADMUSIC import app
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.database import set_video_limit
-from BADMUSIC.utils.decorators.language import language
+from UCHIHA import app
+from UCHIHA.misc import SUDOERS
+from UCHIHA.utils.database import set_video_limit
+from UCHIHA.utils.decorators.language import language
 
 VIDEOLIMIT_COMMAND = get_command("VIDEOLIMIT_COMMAND")
 
