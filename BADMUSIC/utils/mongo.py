dffe3582f5da98117d917ec9d9d5ff0aa9c6e88f
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 22:06:22 +0530

    Update mongo.py

--- BADMUSIC/utils/mongo.py
+++ BADMUSIC/utils/mongo.py
@@ -7,7 +7,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.BADMUSIC
+db = mongo.UCHIHA
 
 coupledb = db.couple
 
