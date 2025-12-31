29c8231babffe876aaaa3b042c53b6fd9b11b8da
UCHIHABOTS <txkuze@gmail.com>
2025-12-31 22:50:20 +0530

    Update active.py

--- BADMUSIC/plugins/tools/active.py
+++ BADMUSIC/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from unidecode import unidecode
 
-from BADMUSIC import app
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.database import (
+from UCHIHA import app
+from UCHIHA.misc import SUDOERS
+from UCHIHA.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
