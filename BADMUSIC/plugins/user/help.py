5a3230c786c10e2233657861de2e9499bca87013
UCHIHABOTS <txkuze@gmail.com>
2026-01-01 17:41:00 +0530

    Update help.py

--- BADMUSIC/plugins/user/help.py
+++ BADMUSIC/plugins/user/help.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from BADMUSIC.misc import SUDOERS as SUDO_USER
+from UCHIHA.misc import SUDOERS as SUDO_USER
 from config import *
 
 R = "MUSICBOT COMMAND"
