5dd898d721843f13eb1c1d58775d3dce3e707968
UCHIHABOTS <txkuze@gmail.com>
2026-01-01 17:45:36 +0530

    Update hiraid.py

--- BADMUSIC/plugins/user/hiraid.py
+++ BADMUSIC/plugins/user/hiraid.py
@@ -15,10 +15,10 @@ from UCHIHA.cplugin.utils.data import RAID, PBIRAID, OneWord, HIRAID, PORM, EMOJ
 
 @Client.on_message(filters.command("hiraid", prefixes=".") & SUDO_USER)
 async def raid(Client: Client, m: Message):  
-      Bad = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
-      if len(Bad) == 2:
-        counts = int(Bad[0])
-        username = Bad[1]
+      ARJUN = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
+      if len(ARJUN) == 2:
+        counts = int(ARJUN[0])
+        username = ARJUN[1]
         if not counts:
           await m.reply_text(f"HIRAID LIMIT NOT FOUND PLEASE GIVE COUNT!")
           return       
@@ -26,12 +26,12 @@ async def raid(Client: Client, m: Message):
           await m.reply_text("you need to specify an user! Reply to any user or gime id/username")
           return
         try:
-           user = await Client.get_users(Bad[1])
+           user = await Client.get_users(ARJUN[1])
         except:
            await m.reply_text("**Error:** User not found or may be deleted!")
            return
       elif m.reply_to_message:
-        counts = int(Bad[0])
+        counts = int(ARJUN[0])
         try:
            user = await Client.get_users(m.reply_to_message.from_user.id)
         except:
